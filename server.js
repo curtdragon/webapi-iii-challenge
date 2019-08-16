d69e21510666e8104b6e781f8b276da6d399a8f0
curtdragon <tealturboser@gmail.com>
2019-08-15 19:42:39 -0500

    Added custom logger middleware and prepped for deploy to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const express = 'express';
-
+const express = require("express");
+const helmet = require("helmet");
 const server = express();
+
+server.use(helmet());
+server.unsubscribe(express.json());
+server.use(logger);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -9,7 +13,13 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
+  const today = new Date();
+  const date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+  const time = today.getHours() + ":" + today.getMinutes();
+  const timeStamp = date + ' ' + time;
 
+  console.log(`${req.method} Request from URL:`, `${req.protocol}://${req.get('host')}${req.originalUrl}`, `Requested at ${timeStamp}`);
+  next();
 };
 
 module.exports = server;
